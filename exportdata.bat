dfd24b3156b79c0055588f3bc207edbd1012531d
fabricio.costa@moldeointeractive.com.ar <fabricio.costa@moldeointeractive.com.ar>
2016-09-28 17:41:49 -0300

    adding basic, midi, sounds, samples, taller examples and exportdata.bat script for windows packages

--- exportdata.bat
+++ exportdata.bat
@@ -1,3 +1,13 @@
-git archive --format zip --output ../data.zip master:data/
-7z.exe x -y ../data.zip -o../data/
+git archive --format zip --output ../basic.zip master:basic/
+git archive --format zip --output ../sound.zip master:sound/
+git archive --format zip --output ../samples.zip master:samples/
+git archive --format zip --output ../midi.zip master:midi/
+git archive --format zip --output ../scripting.zip master:scripting/
+git archive --format zip --output ../taller.zip master:taller/
+7z.exe x -y ../basic.zip -o../datam/basic
+7z.exe x -y ../sound.zip -o../datam/sound
+7z.exe x -y ../samples.zip -o../datam/samples
+7z.exe x -y ../midi.zip -o../datam/midi
+7z.exe x -y ../taller.zip -o../datam/taller
+7z.exe x -y ../scripting.zip -o../datam/scripting
 exit
